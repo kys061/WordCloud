fa93ba4ef85a17f3daf01af3645317d1754389c2
yskang <kys061@gmail.com>
2019-05-17 15:54:13 +0900

    add navigation with material UI

---
+++ src/components/AppShell.js
@@ -0,0 +1,56 @@
+import React from 'react';
+// import { Link as RouterLink } from 'react-router-dom';
+// import Link from '@material-ui/core/Link';
+import { withStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Drawer from '@material-ui/core/Drawer';
+import MenuItem from '@material-ui/core/MenuItem';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+
+const styles = {
+    root: {
+        flexGrow: 1,
+    },
+    menuButton: {
+        marginRight: 'auto'
+    }
+}
+
+class AppShell extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            toggle: false
+        };
+    }
+    handleDrawerToggle = () => this.setState({toggle: !this.state.toggle})
+    render() {
+        console.log(this.props)
+        const { classes } = this.props;
+        return (
+            <div>
+                <div className={classes.root}>
+                    <AppBar position="static">
+                        <IconButton className={classes.menuButton} color="inherit" onClick={this.handleDrawerToggle}>
+                            <MenuIcon/>
+                        </IconButton>
+                    </AppBar>
+                    <Drawer open={this.state.toggle}>
+                        <MenuItem onClick={this.handleDrawerToggle}>
+                            홈 화면
+                        </MenuItem>
+                        <MenuItem onClick={this.handleDrawerToggle}>                
+                            텍스트 관리
+                        </MenuItem>
+                        <MenuItem onClick={this.handleDrawerToggle}>
+                            단어 관리
+                        </MenuItem>
+                    </Drawer>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default withStyles(styles)(AppShell);
